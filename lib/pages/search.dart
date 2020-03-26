feb82290fbcfa77a8f96cb419a133511ab7104ba
Paul Hughes <paul.hughes@hughesxploration.com>
2020-03-26 15:45:23 -0500

    Reset Search code , upload func implemented

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -7,59 +6,9 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
-  AppBar buildSearchField() {
-    return AppBar(
-        backgroundColor: Colors.white,
-        title: TextFormField(
-          decoration: InputDecoration(
-            hintText: "Search for a user...",
-            filled: true,
-            prefixIcon: Icon(
-              Icons.account_box,
-              size: 28.0,
-            ),
-            suffixIcon: IconButton(
-              icon: Icon(Icons.clear),
-              onPressed: () => print('cleared'),
-            ),
-          ),
-        ));
-  }
-
-  Container buildNoContent() {
-    final Orientation orientation = MediaQuery.of(context).orientation;
-    return Container(
-      child: Center(
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            SvgPicture.asset(
-              'assets/images/search.svg',
-              height: orientation == Orientation.portrait ? 300.0 : 200.0,
-            ),
-            Text(
-              "Find Users",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w600,
-                fontSize: 60.0,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
-      appBar: buildSearchField(),
-      body: buildNoContent(),
-    );
+    return Text('Search');
   }
 }
 
